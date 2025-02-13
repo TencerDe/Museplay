39f856b9a6f135b776372cb8c3b984fb68e8fc76
TencerDe <tsharma2907@gmail.com>
2025-02-13 15:06:41 +0530

    commit for userprofile

--- music/models.py
+++ music/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 # Create your models here.
 
@@ -11,3 +13,6 @@ class Song(models.Model):
 
     def __str__(self):
         return self.title
+    
+class UserProfile(AbstractUser):
+    pass
--- musicapp/settings.py
+++ musicapp/settings.py
@@ -121,6 +121,9 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+AUTH_USER_MODEL = 'music.UserProfile'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
